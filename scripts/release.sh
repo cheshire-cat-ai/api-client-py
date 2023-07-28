8f32bcb757bc99a0b832a7aa0194d1100b683026
Nicola <nicorb932@hotmail.com>
2023-07-28 23:33:57 +0200

    Bump version to 0.2.0 for release

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,7 +12,7 @@ if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]
     git commit -m "Bump version to $TAG for release" || true && git push
     echo "Creating new git tag $TAG"
     git tag "$TAG" -m "$TAG"
-    git release "$TAG"
+    git-release "$TAG"
 else
     echo "Cancelled"
     exit 1
