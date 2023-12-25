4f4762bb76376a255b1775bd21e9a5778b1161e7
Emanuele Morrone <67059270+Pingdred@users.noreply.github.com>
2023-12-25 18:46:17 +0100

    Update cat_client.py

--- cheshire_cat_api/cat_client.py
+++ cheshire_cat_api/cat_client.py
@@ -43,13 +43,7 @@ class CatClient:
         self.settings = None
         self.llm = None
 
-        self._ws = WebSocketApp(
-            url,
-            on_message=self.on_ws_message,
-            on_error=self.on_ws_error,
-            on_close=self.on_ws_close,
-            on_open=self.on_ws_open
-        )
+        self._ws = None
 
         self._connect_api()
 
@@ -73,6 +67,14 @@ class CatClient:
     def connect_ws(self):
         protocol = "wss" if self._conn_settings.secure_connection else "ws"
         url = f"{protocol}://{self._conn_settings.base_url}:{self._conn_settings.port}/ws/{self._conn_settings.user_id}"
+
+        self._ws = WebSocketApp(
+            url,
+            on_message=self.on_ws_message,
+            on_error=self.on_ws_error,
+            on_close=self.on_ws_close,
+            on_open=self.on_ws_open
+        )
 
         self.conn = Thread(target=self._ws.run_forever)
         self.conn.start()
@@ -114,7 +116,6 @@ class CatClient:
         if callable(self.on_close):
             self.on_close(status_code, msg)
 
-        self.conn.join()
 
     def send(self, message: str, **kwargs):
         """Send a message to WebSocket server using a separate thread"""
@@ -134,4 +135,4 @@ class CatClient:
 
     @property
     def is_ws_connected(self):
-        return self._ws.sock and self._ws.sock.connected
+        return self._ws and self._ws.sock and self._ws.sock.connected
